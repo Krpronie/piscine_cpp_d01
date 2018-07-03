638287b5adc4b13b0bf673b2c065bbbe56456c71
Krpronie <dlhp222@163.com>
2018-07-03 09:26:19 +0800

    first commit

---
+++ ex01/my_convert_temp.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+ using namespace std;
+
+int main()
+{
+	const unsigned short P = 32;
+	const double R = 5.0 / 9.0;
+ 
+	double tempIn, tempOut;
+	string typeIn, typeOut;
+	cin >> tempIn >> typeIn;
+	cin.ignore(100,'\n');
+	
+	if(typeIn == "Celsius")
+	{
+		tempOut = tempIn / R + P; typeOut = "Fahrenheit"; typeIn = "Celsius";
+		cout.setf(ios::right);
+		cout<<setiosflags(ios::fixed)<<setfill(' ')<<setw(16)<<setprecision(3)<<tempOut<<setw(16)<<typeOut<<endl;
+		return 0;
+	}
+	if(typeIn == "Fahrenheit")
+	{
+		tempOut= (tempIn - P) * R; typeOut = "Celsius"; typeIn = "Fahrenheit";
+		cout.setf(ios::right);
+		cout<<setiosflags(ios::fixed)<<setfill(' ')<<setw(16)<<setprecision(3)<<tempOut<<setw(16)<<typeOut<<endl;
+		return 0;
+	}
+	else
+	{
+		cout << "Invalid Input" << "\n";
+	}
+	return 0;
+}
